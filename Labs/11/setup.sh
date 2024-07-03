6818097e4b0082f32a1849e3e6b86f8512737d05
ricardoaltieri <ricardoaltieri@yahoo.com.br>
2024-07-03 11:45:32 +0100

    Update setup.sh

--- Labs/11/setup.sh
+++ Labs/11/setup.sh
@@ -29,8 +29,8 @@ az configure --defaults workspace=$WORKSPACE_NAME
 
 # Create compute instance
 echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
-az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type ComputeInstance 
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS2_V2 --type ComputeInstance 
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS2_V2 --max-instances 2 --type AmlCompute 
